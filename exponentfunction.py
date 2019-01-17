eab3a8303a55239a66b7fc38ab216975f9d1ff1b
lisamona406 <40734496+lisamona406@users.noreply.github.com>
2019-01-17 20:05:27 +0600

    ModifiedExponentFunction

--- exponentfunction.py
+++ exponentfunction.py
@@ -15,4 +15,4 @@ def raise_to_power(base_num,pow_num):
     for index in range(pow_num):
         result=result*base_num
     return result
-print(raise_to_power(base_num,pow_num))
+print("the result is :"+ str(raise_to_power(base_num,pow_num)))
